8671af05a1bacdf250802f3a4fb61bc462d96fe6
Léo Belzile <belzilel@gmail.com>
2021-04-29 12:27:21 -0400

    Mise à jour chapitre 5

--- _build.sh
+++ _build.sh
@@ -2,7 +2,6 @@
 
 set -ev
 
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook', 	encoding = 'UTF-8')"
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book', 	encoding = 'UTF-8')"
 
